f39e740c3f04d1e9ea3bc415aeb27087bb998f40
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-11 22:40:10 +0500

    Add files via upload

---
+++ Final Keyword/FinalVariableExample.java
@@ -0,0 +1,11 @@
+
+
+class FinalVariableExample
+{
+	public static void main(String [] args)
+	{
+		final int i=10;
+		    //i=i+20; we use this line then error come,bcz of "final" keyword.
+		System.out.println("Output: "+i);  // Output: 10
+	}
+}
